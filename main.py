384b8dc4c0d32c6820a6a8036271102e87b5e2cc
royaboya <94084201+royaboya@users.noreply.github.com>
2024-10-04 02:31:53 -0400

    add browser gps lookup for an individual imaage

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import os
 import argparse
 import PIL.ExifTags
+
+from PIL.ExifTags import GPSTAGS
+
 import PIL.Image
 import PIL.ImageChops
 from PIL.ExifTags import Base
 
+from search import geo_search
+
 parser = argparse.ArgumentParser()
 
-# TODO: add option for explaining exiftags
+# TODO: add option for explaining exiftags?
+# TODO: check if exif data even exists?
+# maybe check file types too idk
 """
 general info like file name, file size, etc
 """
@@ -15,13 +22,18 @@ general info like file name, file size, etc
 def set_arguments():
     INPUT_IMAGE_TEXT = "help-image"
     OUTPUT_FILE_TEXT = "output file name"
-    DIRECTORY = "output directory, default is ..."
+    DIRECTORY = "output directory, default is ./out"
+
+    SEARCH_FLAG = "search GPS on maps"
     
     parser.add_argument("-i", "--input-image", help=INPUT_IMAGE_TEXT)    
     parser.add_argument("-o", "--output-file", help=OUTPUT_FILE_TEXT)
     parser.add_argument("-d","--directory", help=DIRECTORY)
 
-    #parser.add_argument('-g', action='store_true')
+    parser.add_argument("-s", action="store_true", help=SEARCH_FLAG)
+   
+    # add search option from default webbrowser?
+    # add option to do multiple photos
 
 def main():
     set_arguments()
@@ -30,6 +42,8 @@ def main():
 
     output_content = []
     
+    # maybe save exif data globally and apend to output in some other if statement
+    exif_data = {}
 
     # input image is path
     if args.input_image:
@@ -55,13 +69,41 @@ def main():
     if args.directory:
         path = args.directory
         pass
-    
+
+
+    if args.s:
+        # if both work then run search
+        GPS_EXISTS = (34853 in [k for k in exif_data.keys()])
+
         
+        if not(args.input_image):
+            parser.error("--search requires --input-image")
+        else:
+            # gets gps data and convert to readable keys
+            gps_info = {GPSTAGS.get(tag, tag): value for tag,
+                         value in exif_data.get_ifd(34853).items()}
+            
+            #for key in gps_info:
+            #   print(f"{key}: {gps_info[key]}")
+               
+            latitude = gps_info["GPSLatitude"]
+            longitude = gps_info["GPSLongitude"]
+            latitude_ref = gps_info["GPSLatitudeRef"]
+            longitude_ref = gps_info["GPSLongitudeRef"]
+            
+            # dms wont work so will have to convert to decimal 
+            geo_search(latitude, longitude, latitude_ref, longitude_ref)
+         
+        if not(GPS_EXISTS):
+            print("gps info not available, TODO: fix")        
+        
+        #geo_search()
+
     else:
         print(join_content(output_content))
 
 
-def add_general():
+def add_general_section():
     pass
 
     
---
+++ search.py
@@ -0,0 +1,35 @@
+import webbrowser as browser
+
+GOOGLE_MAPS_URL = "https://www.google.com/maps"
+DEGREE_ENCODING = "%C2%B0"
+DOUBLE_QUOTE_ENCODING = "%22"
+
+# assume as tuple
+def geo_search(latitude:tuple, longitude:tuple, ref_a, ref_b):
+    latitude_degree = latitude[0]
+    latitude_minute = latitude[1]
+    latitude_second = latitude[2] 
+    
+    longitude_degree = longitude[0]
+    longitude_minute = longitude[1] 
+    longitude_second = longitude[2]
+    
+    latitude_decimal = dms_to_decimal(latitude_degree, latitude_minute, latitude_second, ref_a)
+    longitude_decimal = dms_to_decimal(longitude_degree, longitude_minute, longitude_second, ref_b)
+    
+    url_a = GOOGLE_MAPS_URL
+    url_b = f"/@{latitude_decimal},{longitude_decimal},17z"
+    
+    #print(url_a + url_b)
+    browser.open(url_a + url_b)
+ 
+ 
+def dms_to_decimal(deg, min, sec, ref):
+    decimal_conversion = round(deg + (float(min)/60) + (float(sec)/3600), 6) # move rounding to string format later
+    if ref in ["W", "S"]:
+        decimal_conversion *= -1
+    return decimal_conversion
+ 
+ 
+def geo_search_degrees():
+    pass
