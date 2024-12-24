a844f84359fe78536fa53bb9e45cb28f60ca582d
royaboya <94084201+royaboya@users.noreply.github.com>
2024-12-23 19:37:12 -0500

    add basic -g display and moved gps flag check

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ def set_arguments():
     
 
     parser.add_argument("-s", action="store_true", help=SEARCH_FLAG)
+    
+    # this option to display GPS info if it exists
     parser.add_argument("-g", action="store_true", help=GPS_FLAG)
    
 
@@ -33,24 +35,27 @@ def main():
     set_arguments()
 
     args = parser.parse_args()
-
     output_content = []
-    
     exif_data = {}
-
-    # input image is path
+    
+    GPS_EXISTS = False;
+    
     if args.input_image:
         if not(os.path.isfile(args.input_image)):
             raise FileNotFoundError
 
         with PIL.Image.open(args.input_image) as input_image:
             exif_data = input_image.getexif()
-            
-            header = f"EXIFT Summary for {args.input_image}:\n"
-            
+                       
+            GPS_EXISTS = (34853 in [k for k in exif_data.keys()])                       
+                            
+                        
+            header = f"EXIF Metadata Summary for {args.input_image}:\n"
             output_content += create_section_line(len(header))
             output_content += header
             output_content += create_section_line(len(header))
+            
+            
             for(exif_tag, v) in exif_data.items():
                 v_converted = str(v)
                 formatted_line = string_formatter(Base(exif_tag).name, v_converted)
@@ -64,8 +69,9 @@ def main():
             # TODO: handle file info in separate method
             output_content += string_formatter("File Name", os.path.basename(args.input_image))
             output_content += string_formatter("File Size", os.path.getsize(args.input_image))
-            output_content += string_formatter("Image Height", input_image.height)
-            output_content += string_formatter("Image Width", input_image.width)
+            # todo: combine hxw into one display
+            output_content += string_formatter("Image Height (px)", input_image.height)
+            output_content += string_formatter("Image Width (px)", input_image.width)
 
 
     if args.output_file:
@@ -87,22 +93,29 @@ def main():
         except OSError as error:
             print(f"Error: {error}" )
 
-    if args.s:
-        # if both work then run search
-        GPS_EXISTS = (34853 in [k for k in exif_data.keys()])
+    
+    if args.s or args.g:
+        gps_output = ""
         
+        if not(args.input_image):
+            parser.error("option requires --input-image")
         
         if not(GPS_EXISTS):
             print(f"GPS Information does not exist for {args.input_image}")
-            return      
+            return
         
-        if not(args.input_image):
-            parser.error("--search requires --input-image")
-        else:
-            # gets gps data and convert to readable keys
-            gps_info = {GPSTAGS.get(tag, tag): value for tag,
-                         value in exif_data.get_ifd(34853).items()}
+        gps_info = {GPSTAGS.get(tag, tag): value for tag,
+                            value in exif_data.get_ifd(34853).items()}
+
+        if args.s:
+            # if both work then run search
+            if not(GPS_EXISTS):
+                print(f"GPS Information does not exist for {args.input_image}")
+                return      
             
+            if not(args.input_image):
+                parser.error("--search requires --input-image")
+                
             try: 
                 latitude = gps_info["GPSLatitude"]
                 longitude = gps_info["GPSLongitude"]
@@ -111,16 +124,23 @@ def main():
             
                 geo_search(latitude, longitude, latitude_ref, longitude_ref)
             except:
-                print("Not enough GPS info found")
-         
-   
+                print("Not enough GPS info found to perform a google search")
+                
+        if args.g:
+            for k, v in dict(gps_info).items():
+              gps_output += string_formatter(k, str(v))
 
+            print(gps_output)
+    
     else:
         print(join_content(output_content))
 
 
+'''
+String formats for aligning by column, should take in two strings ideally
+'''
 def string_formatter(key, value):    
-    return f"{key:<{20}} {value:<{30}}\n"
+    return f"{key:<{20}}: {value:<{30}}\n"
 
 
 
@@ -131,7 +151,7 @@ def write_to_file(file_name, content, directory):
     with open(dir, "w") as file:
         file.write(content)    
     
-    
+# TODO: remove & replace with formatting
 def create_section_line(n):
     if n > 40:
         n = 40
